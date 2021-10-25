2573802b75033c610e6c8f94a2ca814d14132cca
jchima <91480444+jchima@users.noreply.github.com>
2021-10-25 11:12:07 -0600

    changed author to submittable

--- main.go
+++ main.go
@@ -205,7 +205,7 @@ func main() {
 				AuthorName: envOr(EnvGithubActor, ""),
 				AuthorLink: os.Getenv("GITHUB_SERVER_URL") + "/" + os.Getenv(EnvGithubActor),
 				AuthorIcon: os.Getenv("GITHUB_SERVER_URL") + "/" + os.Getenv(EnvGithubActor) + ".png?size=32",
-				Footer:     envOr(EnvSlackFooter, "<https://github.com/rtCamp/github-actions-library|Powered By rtCamp's GitHub Actions Library>"),
+				Footer:     envOr(EnvSlackFooter, "<https://github.com/submittable/github-actions-library|Powered By Submittable's GitHub Actions Library>"),
 				Fields:     fields,
 			},
 		},
